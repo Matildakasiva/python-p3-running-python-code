648248f7f563212a17c55ea40caaf9f60b1739b3
Matilda <ogarimatilda@gmail.com>
2024-05-20 16:41:46 +0300

    add app.py

---
+++ lib/app.py
@@ -0,0 +1,10 @@
+print("Hello world!")
+print("Hello sun!")
+print("Hello sky!")
+
+# Print() is a built-in python function- output a string of text to the terminal [Equivalent to console.log() in JavaScript]
+
+print("Hello world!", end=" ")
+print("Hello sun!", end="!! ")
+print("Hello sky!", end="!!!\n")
+# the end parameter is accommodated by print(), used when one doesn't need a newline character after every sentence
